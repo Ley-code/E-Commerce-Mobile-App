ba41c8c3cdb64118badcca5f7b2b6158e91896f7
Leykun Birhanu <leykunbi12@gmail.com>
2024-08-15 09:39:51 +0300

    added bloc and run unit test for each of the events

--- mobile/Leykun_Birhanu/e_commerce_app_clean/lib/core/usecase.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dartz/dartz.dart';
-
-import 'error/failure.dart';
-
-abstract class UseCase<T, P> {
-  Future<Either<Failure, T>> execute(P p);
-}
--- mobile/Leykun_Birhanu/e_commerce_app_clean/lib/features/product/domain/usecases/get_product_usecase.dart
+++ mobile/Leykun_Birhanu/e_commerce_app_clean/lib/features/product/domain/usecases/get_product_usecase.dart
@@ -1,17 +1,26 @@
 import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
 
 import '../../../../core/error/failure.dart';
-import '../../../../core/usecase.dart';
+import '../../../../core/usecase/usecase.dart';
 import '../entities/product_entity.dart';
 import '../repository/product_repository.dart';
 
-class GetProductUsecase implements UseCase<ProductEntity, String> {
+class GetProductUsecase implements UseCase<ProductEntity, GetParams> {
   final ProductRepository repository;
 
   GetProductUsecase(this.repository);
 
   @override
-  Future<Either<Failure,ProductEntity>> execute(String id) async {
-    return await repository.getProduct(id);
+  Future<Either<Failure, ProductEntity>> call(GetParams params) async {
+    return await repository.getProduct(params.id);
   }
+}
+
+class GetParams extends Equatable {
+  final String id;
+
+  const GetParams({required this.id});
+  @override
+  List<Object?> get props => [id];
 }
